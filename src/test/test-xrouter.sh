e59d4fcbb235443b9fe176cf915182da5dcb78cb
Daniel A. Zorin <jfaranime@gmail.com>
2018-11-23 02:41:03 +0300

    Xrouter test script
    Signed-off-by: Daniel A. Zorin <jfaranime@gmail.com>

---
+++ src/test/test-xrouter.sh
@@ -0,0 +1,96 @@
+set -x
+./blocknetdx-cli xrGetBlockCount SYS
+./blocknetdx-cli xrGetBlockCount SYS 1
+./blocknetdx-cli xrGetBlockCount SYS a
+./blocknetdx-cli xrGetBlockCount SYS -1
+./blocknetdx-cli xrGetBlockCount SYS 0
+./blocknetdx-cli xrGetBlockCount SYS 1.234
+./blocknetdx-cli xrGetBlockCount
+./blocknetdx-cli xrGetBlockCount SYSCOIN
+./blocknetdx-cli xrGetBlockHash SYS 275637
+./blocknetdx-cli xrGetBlockHash SYS 275637 1
+./blocknetdx-cli xrGetBlockHash SYS 275637 a
+./blocknetdx-cli xrGetBlockHash SYS 275637 -1
+./blocknetdx-cli xrGetBlockHash SYS 275637 0
+./blocknetdx-cli xrGetBlockHash SYS 275637 1.234
+./blocknetdx-cli xrGetBlockHash
+./blocknetdx-cli xrGetBlockHash SYS
+./blocknetdx-cli xrGetBlockHash SYSCOIN 275637
+./blocknetdx-cli xrGetBlockHash SYS aaa
+./blocknetdx-cli xrGetBlock SYS 7a0d278e1962a251a1484e3c583675ae0bc7bfff5d8fa8eb4cfabcfdfe8808db
+./blocknetdx-cli xrGetBlock SYS 7a0d278e1962a251a1484e3c583675ae0bc7bfff5d8fa8eb4cfabcfdfe8808db 1
+./blocknetdx-cli xrGetBlock SYS 7a0d278e1962a251a1484e3c583675ae0bc7bfff5d8fa8eb4cfabcfdfe8808db a
+./blocknetdx-cli xrGetBlock SYS 7a0d278e1962a251a1484e3c583675ae0bc7bfff5d8fa8eb4cfabcfdfe8808db -1
+./blocknetdx-cli xrGetBlock SYS 7a0d278e1962a251a1484e3c583675ae0bc7bfff5d8fa8eb4cfabcfdfe8808db 0
+./blocknetdx-cli xrGetBlock SYS 7a0d278e1962a251a1484e3c583675ae0bc7bfff5d8fa8eb4cfabcfdfe8808db 1.234
+./blocknetdx-cli xrGetBlock
+./blocknetdx-cli xrGetBlock SYS
+./blocknetdx-cli xrGetBlock SYSCOIN 7a0d278e1962a251a1484e3c583675ae0bc7bfff5d8fa8eb4cfabcfdfe8808db
+./blocknetdx-cli xrGetBlock SYS aaa
+./blocknetdx-cli xrGetTransaction SYS 91a19a794261041fb64b003514c0ab59d69bd19f6e53a5a0f0c2da000d286f39
+./blocknetdx-cli xrGetTransaction SYS 91a19a794261041fb64b003514c0ab59d69bd19f6e53a5a0f0c2da000d286f39 1
+./blocknetdx-cli xrGetTransaction SYS 91a19a794261041fb64b003514c0ab59d69bd19f6e53a5a0f0c2da000d286f39 a
+./blocknetdx-cli xrGetTransaction SYS 91a19a794261041fb64b003514c0ab59d69bd19f6e53a5a0f0c2da000d286f39 -1
+./blocknetdx-cli xrGetTransaction SYS 91a19a794261041fb64b003514c0ab59d69bd19f6e53a5a0f0c2da000d286f39 0
+./blocknetdx-cli xrGetTransaction SYS 91a19a794261041fb64b003514c0ab59d69bd19f6e53a5a0f0c2da000d286f39 1.234
+./blocknetdx-cli xrGetTransaction
+./blocknetdx-cli xrGetTransaction SYS
+./blocknetdx-cli xrGetTransaction SYSCOIN 91a19a794261041fb64b003514c0ab59d69bd19f6e53a5a0f0c2da000d286f39
+./blocknetdx-cli xrGetTransaction SYS aaa
+./blocknetdx-cli xrGetBlocks SYS 275670
+./blocknetdx-cli xrGetBlocks SYS 275670 1
+./blocknetdx-cli xrGetBlocks SYS 275670 a
+./blocknetdx-cli xrGetBlocks SYS 275670 -1
+./blocknetdx-cli xrGetBlocks SYS 275670 0
+./blocknetdx-cli xrGetBlocks SYS 275670 1.234
+./blocknetdx-cli xrGetBlocks
+./blocknetdx-cli xrGetBlocks SYS
+./blocknetdx-cli xrGetBlocks SYSCOIN
+./blocknetdx-cli xrGetBlocks SYS aaa
+./blocknetdx-cli xrGetTransactions SYS SYLs4rjz2gWWniNcfiJNsi3h75r3vuvwYG 275670
+./blocknetdx-cli xrGetTransactions SYS SYLs4rjz2gWWniNcfiJNsi3h75r3vuvwYG 275670 1
+./blocknetdx-cli xrGetTransactions SYS SYLs4rjz2gWWniNcfiJNsi3h75r3vuvwYG 275670 a
+./blocknetdx-cli xrGetTransactions SYS SYLs4rjz2gWWniNcfiJNsi3h75r3vuvwYG 275670 -1
+./blocknetdx-cli xrGetTransactions SYS SYLs4rjz2gWWniNcfiJNsi3h75r3vuvwYG 275670 0
+./blocknetdx-cli xrGetTransactions SYS SYLs4rjz2gWWniNcfiJNsi3h75r3vuvwYG 275670 1.234
+./blocknetdx-cli xrGetTransactions
+./blocknetdx-cli xrGetTransactions SYS
+./blocknetdx-cli xrGetTransactions SYSCOIN
+./blocknetdx-cli xrGetTransactions SYS aaa
+./blocknetdx-cli xrGetTransactions SYS SYLs4rjz2gWWniNcfiJNsi3h75r3vuvwYG aaa
+./blocknetdx-cli xrGenerateBloomFilter SjpmLTUcS1DycBUx1frYBisMEHt7vNFygQ
+./blocknetdx-cli xrGenerateBloomFilter
+./blocknetdx-cli xrGenerateBloomFilter aaa
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS 0000080004 275660
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS 0000080004 275660 1
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS 0000080004 275660 a
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS 0000080004 275660 -1
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS 0000080004 275660 0
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS 0000080004 275660 1.234
+./blocknetdx-cli xrGetTransactionsBloomFilter
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS
+./blocknetdx-cli xrGetTransactionsBloomFilter SYSCOIN 0000080004 275660
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS aaa 275660
+./blocknetdx-cli xrGetTransactionsBloomFilter SYS 0000080004 aaa
+./blocknetdx-cli xrGetReply DE5A196E-5304-4C61-AD09-F3C960A56C07
+./blocknetdx-cli xrGetReply
+./blocknetdx-cli xrGetReply aaa
+./blocknetdx-cli xrSendTransaction SYS 0200000001ec8b978729f3253066494e81f263afbe11e7d4222a8335c4c582c2dacf1a8ab5010000006b48304502210081f8ee69ed982df674418c07c2d4a8c5967078f339e8f863fc5b8c86b6bbd69802204058f87b55752616a27ea607cd9de1b949f01b439d1682101e836fdb6667f8e701210323f7e071df863cf20ce13613c68579cdedb6d7c6cf3912f26dac53ec4309c777ffffffff0120a10700000000001976a914eff8cb97723237fe3059774d2a66d02f936e1f1188ac00000000
+./blocknetdx-cli xrSendTransaction
+./blocknetdx-cli xrSendTransaction SYSCOIN 0200000001f0ff654d64857c87fa0ee8da6d8836564e1be62ea3b6d1aef7039cb012c1da59000000006a47304402205e6e25bf702f580b8a31dcb2c0e2dd34df44f3b7cf083fa0ca8a2fd71b81080302201cc4ef54b8dcce41a30d9a847fb61cf20fefe66d07f4896dc90ffe8b6c617976012102c470fb419f4747ac3a61f0a05af63a348ac2640c467c7a6138cd9fdb6292f417ffffffff01a0bb0d00000000001976a9149db11b8a7c0afc7f0a18b4716eb59626ac51c6f088ac00000000
+./blocknetdx-cli xrSendTransaction SYS aaa
+./blocknetdx-cli xrStatus
+./blocknetdx-cli xrStatus aaa
+./blocknetdx-cli xrGetBalanceUpdate SYS SVCnbqUAJcMLnwTuJ3N6Zfm1xrDxN6PBht 275705
+./blocknetdx-cli xrGetBalanceUpdate SYS SVCnbqUAJcMLnwTuJ3N6Zfm1xrDxN6PBht 275705 1
+./blocknetdx-cli xrGetBalanceUpdate SYS SVCnbqUAJcMLnwTuJ3N6Zfm1xrDxN6PBht 275705 a
+./blocknetdx-cli xrGetBalanceUpdate SYS SVCnbqUAJcMLnwTuJ3N6Zfm1xrDxN6PBht 275705 -1
+./blocknetdx-cli xrGetBalanceUpdate SYS SVCnbqUAJcMLnwTuJ3N6Zfm1xrDxN6PBht 275705 0
+./blocknetdx-cli xrGetBalanceUpdate SYS SVCnbqUAJcMLnwTuJ3N6Zfm1xrDxN6PBht 275705 1.234
+./blocknetdx-cli xrGetBalanceUpdate
+./blocknetdx-cli xrGetBalanceUpdate SYS
+./blocknetdx-cli xrGetBalanceUpdate SYSCOIN
+./blocknetdx-cli xrGetBalanceUpdate SYS aaa 275705
+./blocknetdx-cli xrGetBalanceUpdate SYS SVCnbqUAJcMLnwTuJ3N6Zfm1xrDxN6PBht aaa
+./blocknetdx-cli xrGetBlockCount SYS 5
+set +x
