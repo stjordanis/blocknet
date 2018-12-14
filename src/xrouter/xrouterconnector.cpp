074bb82a932eb9d7c0e09b90f0c1d82cfd9f2717
Daniel A. Zorin <jfaranime@gmail.com>
2018-12-14 08:32:22 +0700

    clear xrouterconnector.cpp
    Signed-off-by: Daniel A. Zorin <jfaranime@gmail.com>

--- src/xrouter/xrouterconnector.cpp
+++ src/xrouter/xrouterconnector.cpp
@@ -1,49 +1,7 @@
 #include "xrouterconnector.h"
-#include "xrouterlogger.h"
-#include "xrouterdef.h"
-#include "xroutererror.h"
-
-#include <cstdio>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <array>
-#include <ctime>
-#include "json/json_spirit_reader_template.h"
-#include "json/json_spirit_writer_template.h"
-#include "json/json_spirit_utils.h"
-
-#include <boost/asio.hpp>
-#include <boost/iostreams/concepts.hpp>
-#include <boost/iostreams/stream.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/asio/ssl.hpp>
-#include <boost/locale.hpp>
-#include <boost/archive/iterators/base64_from_binary.hpp>
-#include <boost/archive/iterators/binary_from_base64.hpp>
-#include <boost/archive/iterators/insert_linebreaks.hpp>
-#include <boost/archive/iterators/transform_width.hpp>
-#include <boost/archive/iterators/ostream_iterator.hpp>
-
-#include <stdio.h>
-
-#include "rpcserver.h"
-#include "rpcprotocol.h"
-#include "rpcclient.h"
-#include "base58.h"
-#include "wallet.h"
-#include "init.h"
-#include "key.h"
-#include "core_io.h"
 
 namespace xrouter
 {
-
-using namespace json_spirit;
-using namespace std;
-using namespace boost;
-using namespace boost::asio;
 
 WalletConnectorXRouter::WalletConnectorXRouter()
 {
