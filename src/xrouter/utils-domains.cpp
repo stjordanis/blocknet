fc92839740e2a2b1d41019f62b22aee5a289727d
Daniel A. Zorin <jfaranime@gmail.com>
2018-12-14 07:59:33 +0700

    Verify domain function moved to utils
    Signed-off-by: Daniel A. Zorin <jfaranime@gmail.com>

--- src/xrouter/utils-domains.cpp
+++ src/xrouter/utils-domains.cpp
@@ -2,6 +2,7 @@
 //******************************************************************************
 #include "xrouterutils.h"
 #include "xrouterdef.h"
+#include "xrouterlogger.h"
 
 #include "json/json_spirit_reader_template.h"
 #include "json/json_spirit_writer_template.h"
@@ -39,5 +40,68 @@ std::string generateDomainRegistrationTx(std::string domain, std::string addr) {
     sendTransactionBlockchain(raw_tx, txid);
     return txid;
 }
+
+bool verifyDomain(std::string tx, std::string domain, std::string addr, int& blockNumber)
+{
+    uint256 txHash;
+    txHash.SetHex(tx);
+    CTransaction txval;
+    uint256 hashBlock;
+    CCoins coins;
+    std::vector<CTxOut> vout_list;
+    if (!pcoinsTip->GetCoins(txHash, coins)) {
+        LOG() << "Output spent for tx: " << tx;
+        return false;
+    }
     
+    GetTransaction(txHash, txval, hashBlock, true);
+    vout_list = txval.vout;
+    CBlockIndex* pblockindex = mapBlockIndex[hashBlock];
+    blockNumber = pblockindex->nHeight;
+
+    bool has_domain = false;
+    bool has_deposit = false;
+    for (CTxOut txOut : vout_list) {
+        std::string script = HexStr(txOut.scriptPubKey);
+
+        // TODO 6a == OP_RETURN, maybe a more careful check is needed here?
+        if (txOut.nValue == 0) {
+            // This is the txout that stores the domain name
+            if (script.substr(0, 2) != "6a") {
+                return false;
+            }
+            vector<unsigned char> data = ParseHex(script.substr(4));
+            std::string extracted_domain = std::string(data.begin(), data.end());
+            if ("blocknet://" + domain != extracted_domain)
+                return false;
+            
+            has_domain = true;
+            LOG() << "Extracted domain: "  << extracted_domain;
+        } else if (txOut.nValue == to_amount(XROUTER_DOMAIN_REGISTRATION_DEPOSIT)) {
+            // This is the deposit txout, should be to service node payout address
+            CTxDestination destination;
+            if (!ExtractDestination(txOut.scriptPubKey, destination)) {
+                LOG() << "Unable to extract destination";
+                continue;
+            }
+            
+            auto keyid = boost::get<CKeyID>(&destination);
+            if (!keyid) {
+                LOG() << "Destination must be a single address";
+                continue;
+            }
+            
+            std::string extracted_addr = CBitcoinAddress(*keyid).ToString();
+            
+            LOG() << "Extracted address: "  << extracted_addr;
+            if (extracted_addr != addr)
+                continue;
+            
+            has_deposit = true;
+        }
+    }
+    
+    return has_domain && has_deposit;
+}
+
 } // namespace xrouter
--- src/xrouter/xrouterutils.h
+++ src/xrouter/xrouterutils.h
@@ -42,14 +42,17 @@ public:
         return true;
     }
 };
-    
+
+
+// Network and RPC interface
 std::string CallCMD(std::string cmd);
 std::string CallURL(std::string ip, std::string port, std::string url);
-
 Object CallRPC(const std::string & rpcuser, const std::string & rpcpasswd,
                const std::string & rpcip, const std::string & rpcport,
                const std::string & strMethod, const Array & params);
 
+
+// Payment functions
 bool createAndSignTransaction(std::string address, CAmount amount, std::string & raw_tx);
 bool createAndSignTransaction(boost::container::map<std::string, CAmount> addrs, string & raw_tx);
 bool createAndSignTransaction(Array txparams, std::string & raw_tx);
@@ -58,6 +61,9 @@ std::string signTransaction(std::string& raw_tx);
 bool sendTransactionBlockchain(std::string raw_tx, std::string & txid);
 bool sendTransactionBlockchain(std::string address, CAmount amount, std::string & raw_tx);
 CMutableTransaction decodeTransaction(std::string tx);
+
+
+// Payment channels
 PaymentChannel createPaymentChannel(CPubKey address, CAmount deposit, int date);
 bool createAndSignChannelTransaction(PaymentChannel channel, std::string address, CAmount deposit, CAmount amount, std::string & raw_tx);
 bool verifyChannelTransaction(std::string transaction);
@@ -65,15 +71,22 @@ bool finalizeChannelTransaction(PaymentChannel channel, CKey snodekey, std::stri
 std::string createRefundTransaction(PaymentChannel channel);
 double getTxValue(std::string rawtx, std::string address, std::string type="address");
 int getChannelExpiryTime(std::string rawtx);
+
+
+// Domains
 std::string generateDomainRegistrationTx(std::string domain, std::string addr);
-CAmount to_amount(double val);
-bool is_number(std::string s);
-bool is_hash(std::string s);
-bool is_address(std::string s);
+bool verifyDomain(std::string tx, std::string domain, std::string addr, int& blockNumber);
+
 
 // Signing & Verification of packets
 bool satisfyBlockRequirement(uint256& txHash, uint32_t& vout, CKey& key);
 bool verifyBlockRequirement(const XRouterPacketPtr& packet);
+
+// Miscellaneous functions
+CAmount to_amount(double val);
+bool is_number(std::string s);
+bool is_hash(std::string s);
+bool is_address(std::string s);
 
 } // namespace
 
